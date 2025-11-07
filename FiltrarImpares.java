99a1289be3ff845d8a746ceffb0758fdf4aa9203
GarciaMunozCarmen <garciamunozcarmen18@gmail.com>
2025-11-07 14:03:03 +0100

    Update FiltrarImpares.java

--- FiltrarImpares.java
+++ FiltrarImpares.java
@@ -1,10 +1,27 @@
+import java.util.Arrays;
+
 public class FiltrarImpares {
-    public static void main(String[] args) {
-        int[] array = {1,2,3,4,5,6};
-        for (int i = 0; i < array.length; i++) {
-            if(array[i] % 2 != 0){
-                System.out.print(array[i] + " ");
+    public static void comoTexto(int[] entrada){
+        for (int i = 0; i < entrada.length; i++) {
+            if(entrada[i] % 2 != 0){
+                System.out.print(entrada[i] + " ");
             }
         }
+    }
+    public static int[] comoArray(int[] entrada){
+        int[] salida = new int[entrada.length];
+        int indice = 0;
+        for (int i = 0; i < entrada.length; i++) {
+            if(entrada[i] % 2 != 0){
+                salida[indice++] = entrada[i];
+            }
+        }
+        
+        return Arrays.copyOf(salida, indice);
+    }
+    public static void main(String[] args) {
+        int[] array = {1,2,3,4,5,6};
+        comoTexto(array);
+        System.out.println(Arrays.toString(comoArray(array)));
     }
 }
