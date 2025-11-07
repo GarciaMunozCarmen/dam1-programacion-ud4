0274309e1e0ba804d7a264fdf7813b6732c727d0
GarciaMunozCarmen <garciamunozcarmen18@gmail.com>
2025-11-07 13:48:33 +0100

    Update FiltrarImpares.java

--- FiltrarImpares.java
+++ FiltrarImpares.java
@@ -2,9 +2,7 @@ public class FiltrarImpares {
     public static void main(String[] args) {
         int[] array = {1,2,3,4,5,6};
         for (int i = 0; i < array.length; i++) {
-            if(array[i] % 2 == 0){
-                continue;
-            }else{
+            if(array[i] % 2 != 0){
                 System.out.print(array[i] + " ");
             }
         }
